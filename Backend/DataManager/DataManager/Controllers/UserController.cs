d4928a239900430879a3a311506ee686160932fe
vometablissement <hoechstesetablissement@gmail.com>
2019-02-12 19:21:26 +0100

    Added Requests

--- Backend/DataManager/DataManager/Controllers/UserController.cs
+++ Backend/DataManager/DataManager/Controllers/UserController.cs
@@ -25,5 +25,58 @@ namespace TodoApi.Controllers
                 _context.SaveChanges();
             }
         }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserById(long id)
+        {
+            var todoItem = await _context.Users.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(long id, User user)
+        {
+            if (id != user.PersonId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodoItem", new { id = user.PersonId }, user);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> DeleteUser(long id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
     }
 }
