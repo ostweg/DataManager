31e58b4bcfa3cdfdbf590046f544fadbf703a63e
Zian  Wermelinger <zwermi@gmail.com>
2019-02-14 12:01:33 +0100

    css and new component

---
+++ datamanager/src/app/filebrowser/filebrowser.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FilebrowserComponent } from './filebrowser.component';
+
+describe('FilebrowserComponent', () => {
+  let component: FilebrowserComponent;
+  let fixture: ComponentFixture<FilebrowserComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FilebrowserComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FilebrowserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
