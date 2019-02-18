8954f56aa480c4ebf43b38738063cd0eb715c307
Zian  Wermelinger <zwermi@gmail.com>
2019-02-18 08:35:53 +0100

    This was the update of Friday, but it didn't push it correctly, sadly.

--- datamanager/src/app/app.module.ts
+++ datamanager/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilebrowserComponent } from './filebrowser/filebrowser.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'filebrowser', component:FilebrowserComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilebrowserComponent
+    FilebrowserComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
